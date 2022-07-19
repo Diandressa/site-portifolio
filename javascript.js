bbc2f9fe9511010ac59f3979e29cfa9ad6393d8a
Diandressa <andressa_nicolau@hotmail.com>
2022-07-19 03:26:11 -0300

    finalização home port

--- javascript.js
+++ javascript.js
@@ -5,25 +5,48 @@ let ver = document.querySelectorAll('.vermelho');
 
 let tempo = 3000
 let n = 1
+let c = 0
 
 setInterval("carrossel(n)", tempo)
 function carrossel(n){
-	
-	img[n].src = "./imgs/ex" + (n+2) + ".jpg"
-	console.log(n)	 
+
+	img[n].src = "./imgs/ex" + (Math.floor(Math.random()*(7+1 - 1)+1)) + ".jpg"	 
 }
 
-setInterval("cor(n)", tempo)
-function cor(n){
-	n = n-1
-	azul[n].classList.replace('azul', 'vermelho')
-	ver[n].classList.replace('vermelho', 'azul')
+setInterval("cor(c)", tempo)
+function cor(c){
+	
+	console.log(c)
+	if (c == 0){
+		//azul[c].classList.replace('azul', 'vermelho')
+		azul[c].classList.toggle('azul')
+		azul[c].classList.toggle('vermelho')
+
+		ver[c].classList.toggle('vermelho')
+		ver[c].classList.toggle('azul')
+
+	} else {
+		azul[c].classList.toggle('azul')
+		azul[c].classList.toggle('vermelho')
+
+		ver[c].classList.toggle('vermelho')
+		ver[c].classList.toggle('azul')
+	}
+	
 }
 
 setInterval("soma()", tempo)
 function soma(){
-	n++
-	console.log(n)
+	c++
+	if (c >= 2){
+		c = 0
+	}
 }
 
+setInterval("random()", tempo)
+function random(){
+	n = Math.floor(Math.random()*(7+1 - 1)+1)
+}
+
+//console.log(Math.floor(Math.random()*(7+1 - 1)+1))
 
